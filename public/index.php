45d39f4d0cbe8ed279f7696586e92805733e7b19
Eaglewas <daguilera3220@gmail.com>
2019-10-25 15:21:48 -0600

    d

--- public/index.php
+++ public/index.php
@@ -28,11 +28,11 @@ $allowedOrigins = array(
 if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
    foreach ($allowedOrigins as $allowedOrigin) {
       if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
-         header('Access-Control-Allow-Origin: *'); 
-         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS'); 
-         header('Access-Control-Max-Age: 1000'); 
-         header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With'); 
-         break; 
+         // header('Access-Control-Allow-Origin: *');
+         // header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+         // header('Access-Control-Max-Age: 1000');
+         // header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+         break;
       }
    }
 }  
