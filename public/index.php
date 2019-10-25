3deb40186fcebc1fc4d27d4da01762e2783520f0
Eaglewas <daguilera3220@gmail.com>
2019-10-25 14:16:15 -0600

    trying fix cors

--- public/index.php
+++ public/index.php
@@ -1,20 +1,20 @@
 <?php
 
 $allowedOrigins = array(
-    '(http(s)://)?(www\.)?api.piscador', // Laravel API Domain
-    'http://localhost:8080' // VueJS CLient
- );
- if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
-    foreach ($allowedOrigins as $allowedOrigin) {
-       if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
-          header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-          header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-          header('Access-Control-Max-Age: 1000');
-          header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-          break;
-       }
-    }
- }
+'(http(s)://)?(www\.)?api.piscador', // Laravel API Domain
+'http://localhost:8080' // VueJS CLient
+); 
+if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
+foreach ($allowedOrigins as $allowedOrigin) {
+if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
+header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']); 
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS'); 
+header('Access-Control-Max-Age: 1000'); 
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With'); 
+break; 
+}
+}
+}
 
 /*
 |--------------------------------------------------------------------------
@@ -22,12 +22,9 @@ $allowedOrigins = array(
 |--------------------------------------------------------------------------
 |
 | First we need to get an application instance. This creates an instance
-| of the application / container and bootstraps the application so it
-| is ready to receive HTTP / Console requests from the environment.
-|
-*/
+| of the application /container and bootstraps the application so it | is ready to receive HTTP / Console requests from the environment .  |  */
 
-$app = require __DIR__.'/../bootstrap/app.php';
+$app = require __DIR__ . '/../bootstrap/app.php'; 
 
 /*
 |--------------------------------------------------------------------------
@@ -40,9 +37,22 @@ $app = require __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+   // should do a check here to match $_SERVER['HTTP_ORIGIN'] to a
+   // whitelist of safe domains
+   header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+   header('Access - Control - Allow - Credentials:true');
+   header('Access - Control - Max - Age:86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-header("Access-Control-Allow-Origin:*");
-header("Content-Type: application/json");
-header("Access-Control-Allow-Headers: Content-Type");
+   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+       header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+   if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+       header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+}
 
 $app->run();
