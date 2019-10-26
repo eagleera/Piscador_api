489004d344952270636983ddad758bb9746045b2
Eaglewas <daguilera3220@gmail.com>
2019-10-26 13:20:04 -0600

    dasd

--- public/index.php
+++ public/index.php
@@ -28,10 +28,10 @@ $allowedOrigins = array(
 if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != '') {
    foreach ($allowedOrigins as $allowedOrigin) {
       if (preg_match('#' . $allowedOrigin . '#', $_SERVER['HTTP_ORIGIN'])) {
-         // header('Access-Control-Allow-Origin: *');
-         // header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-         // header('Access-Control-Max-Age: 1000');
-         // header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+         header('Access-Control-Allow-Origin: *');
+         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+         header('Access-Control-Max-Age: 1000');
+         header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
          break;
       }
    }
