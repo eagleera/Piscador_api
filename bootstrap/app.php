823d51ea6dafef182a16a3fa34913a9874c15c23
Eaglewas <daguilera3220@gmail.com>
2019-10-26 11:39:23 -0600

    se agrega cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,9 @@ $app->withFacades();
 
 $app->withEloquent();
 
+$app->configure('cors');
+
+
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
@@ -60,6 +63,9 @@ $app->singleton(
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
+$app->middleware([
+    \Barryvdh\Cors\HandleCors::class,
+]);
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
@@ -79,6 +85,7 @@ $app->routeMiddleware([
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Barryvdh\Cors\ServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
 /*
