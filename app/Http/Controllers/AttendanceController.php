f174c31b58e0383639eb1b7e513ac117d3c77bec
Eaglewas <daguilera3220@gmail.com>
2019-10-28 20:19:52 -0700

    se arma el show

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -22,8 +22,7 @@ class AttendanceController extends Controller
     public function index(Request $request)
     {
         $date = $request->input('date');
-        $date = str_replace('/', '-', $date);
-        $date = date('Y-m-d', strtotime($date));
+        $date = DateTime::createFromFormat('d/m/Y', $date)->format('Y-m-d');
         $attendance =  Attendance::where('attendance_day', $date)->get();
         dd($date, $attendance);
         foreach($attendance as $att) {
